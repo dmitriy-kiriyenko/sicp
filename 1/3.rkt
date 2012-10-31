dfa2fc5bc902cbfa46d67c6187010040e601a5d1
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2012-10-31 08:18:48 +0200

    1.1-1.5 (1.4, 1.5 решил в уме)

---
+++ 1/3.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(require rackunit)
+
+(define (square a)  (* a a))
+
+(define (sum-of-squares a b)
+  (+ (square a) (square b)))
+
+(define (sum-of-biggest-squares x y z)
+  (cond ((and (>= x z) (>= y z)) (sum-of-squares x y))
+        ((and (>= x y) (>= z y)) (sum-of-squares x z))
+        (else (sum-of-squares y z))
+   ))
+
+(check-equal? (sum-of-biggest-squares 1 2 3) 13)
+(check-equal? (sum-of-biggest-squares 3 2 1) 13)
+(check-equal? (sum-of-biggest-squares 2 2 2) 8)
+(check-equal? (sum-of-biggest-squares 1 0 0) 1)
