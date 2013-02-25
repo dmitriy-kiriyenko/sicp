41ae9e735b95b47a4927acb14cccbc59dc61f8fc
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2013-02-25 11:47:30 +0200

    Fix zero and refactor to closures.

--- 1/07.rkt
+++ 1/07.rkt
@@ -3,19 +3,23 @@
 (require rackunit)
 
 (define (sqrt x)
-  (define (sqrt-iter guess prev-guess)
-    (if (good-enough? guess prev-guess)
-      guess
-      (sqrt-iter (improve guess)
-                 guess)))
-  (define (good-enough? guess prev-guess)
-    (< (abs (/ (- guess prev-guess) prev-guess)) 0.001))
-  (define (improve guess)
-    (average guess (/ x guess)))
   (define (average x y)
     (/ (+ x y) 2))
-  (sqrt-iter 1.0 0.5))
+  (define (sqrt-iter guess prev-guess)
+    (define (good-enough?)
+      (< (abs (/ (- guess prev-guess) prev-guess)) 0.001))
+    (define (improve)
+      (average guess (/ x guess)))
+    (if (good-enough?)
+      guess
+      (sqrt-iter (improve)
+                 guess)))
+  (if (= x 0)
+      0
+      (sqrt-iter 1.0 0.5))
+  )
 
 (check < (abs (- (sqrt 4) 2)) 0.001)
 (check < (abs (- (sqrt 0.00000001) 0.0001)) 0.001)
+(check < (abs (- (sqrt 0) 0)) 0.001)
 (check < (abs (- (sqrt 1000000000000000) 31622776)) 10)
--- 1/08.rkt
+++ 1/08.rkt
@@ -3,24 +3,26 @@
 (require rackunit)
 
 (define (qbrt x)
-  (define (qbrt-iter guess prev-guess)
-    (if (good-enough? guess prev-guess)
-        guess
-        (qbrt-iter (improve guess)
-                   guess)))
-
-  (define (good-enough? guess prev-guess)
-    (< (abs (/ (- guess prev-guess) prev-guess)) 0.001))
-
   (define (square x) (* x x))
-
-  (define (improve guess)
-    (/ (+ (/ x (square guess))
+  (define (qbrt-iter guess prev-guess)
+    (define (good-enough?)
+      (< (abs (/ (- guess prev-guess) prev-guess)) 0.001))
+    (define (improve)
+      (/ (+ (/ x (square guess))
           (* 2 guess))
        3))
 
-  (qbrt-iter 1.0 0.5))
+    (if (good-enough?)
+        guess
+        (qbrt-iter (improve)
+                   guess)))
+
+  (if (= x 0)
+      0
+      (qbrt-iter 1.0 0.5))
+  )
 
 (check < (abs (- (qbrt 27) 3)) 0.001)
 (check < (abs (- (qbrt 0.000000001) 0.001)) 0.001)
+(check < (abs (- (qbrt 0) 0)) 0.001)
 (check < (abs (- (qbrt 10000000000000000) 215443)) 10)
